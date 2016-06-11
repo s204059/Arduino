a19bf2684bd625f675f1850fd33f6863ca7b6357
s204059 <s204059@studenti.polito.it>
2016-06-11 07:42:25 +0200

    FtB_Arduino v 1.1 (beta)
    tried to fix bugs

--- FtB_Arduino.ino
+++ FtB_Arduino.ino
@@ -1,18 +1,18 @@
 #include "TimerOne.h"
 
 #define photocell_window 1
-#define photocell_door 2
+/* #define photocell_door 2 */
 #define Vcc 7
 #define fan_1 13
 #define fan_2 14
 
 #define waiting_for_sensor 10  /* HIGH if you want the fan to run */
 
-int open_window = 0;        /* 0 -> closed, 1 -> open */
-int open_door = 0;
-int fans_status = 0;        /* 1 if the fans are currently working */
+boolean something_open = false;        /* 0 -> closed, 1 -> open */
+int fans_status = 0;                   /* 1 if the fans are currently working */
 int fans_timeout = 60;
-int immediate_on = 0;       /* if 1 the fans will start running imediately */
+boolean immediate_on = false;          /* if true the fans will start running imediately */
+boolean immediate_off = false;         /* if true the system will shut down */ 
 
 int on_off = 0;
 
@@ -20,7 +20,7 @@ void setup() {
   // put your setup code here, to run once:
 
   pinMode(photocell_window,INPUT);
-  pinMode(photocell_door,INPUT);
+  /* pinMode(photocell_door,INPUT); */
 
   pinMode(Vcc,OUTPUT);
   
@@ -32,35 +32,37 @@ void setup() {
   Timer1.initialize(5000000);           /* initialize timer1, and set a 5 seconds period */
   Timer1.attachInterrupt(sensing);      /* set sensing() as the timer overflow interrupt ISR */
 
-  digitalWrite(photocell_window,HIGH);
-  digitalWrite(photocell_door,HIGH);
+  digitalWrite(photocell_window,HIGH);  /* digitalWrite(x,HIGH) enables pull-up resistor on input x */
+  /* digitalWrite(photocell_door,HIGH); */
 
   digitalWrite(Vcc,HIGH);
 
-  digitalWrite(fan_1,LOW);
+  digitalWrite(fan_1,LOW);              /* digitalWrite(y,LOW) sets to 0V (ground) the voltage on ouput y */
   digitalWrite(fan_2,LOW);
 
-  digitalWrite(waiting_for_sensor,LOW);
+  digitalWrite(waiting_for_sensor,HIGH);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if(on_off == 1 ){
+  if( on_off == 1 and something_open == false ){
     if( fans_status == 0 ){
       digitalWrite(fan_1,HIGH);
       digitalWrite(fan_2,HIGH);
+      
+      fans_status = 1;
     }
-  }
+  } // if on_off is 1, no windows or doors are open and the fans were not running already, they will now start
   
-  if( open_window == 1 or open_door == 1 ){
+  if( something_open == true ){
     digitalWrite(fan_1,LOW);
     digitalWrite(fan_2,LOW);
 
     fans_status = 0;
     fans_timeout = 60;
-  } // The fans won't run if windows or doors are open
+  } // The fans won't run anymore if windows or doors are open, the timeout is returned to defualt value
 
   if( fans_timeout == 0 ){
     digitalWrite(fan_1,LOW);
@@ -70,25 +72,35 @@ void loop() {
     fans_timeout = 60;
   } // this code segment is used to turn off the fans after approximately 5 minutes of continous operation
 
-  if( immediate_on == 1 ){
+  if( immediate_on == true and something_open == false ){
     if( fans_status == 0 ){
       digitalWrite(fan_1,HIGH);
       digitalWrite(fan_2,HIGH);
+
+      fans_status = 1;
     }
-    else
-      fans_timeout == 60;
-  } // This code segments immediately turns on the fans when an immediate action is required
+  } // This code segments immediately turns on the fans (if not already on) when an immediate action is required 
+
+  if( immediate_off == true )
+    Timer1.detachInterrupt();
+  else
+    Timer1.attachInterrupt(sensing);
 }
 
 void sensing(){
   // put the code of Timer1 ISR here:
 
-  on_off = digitalRead(waiting_for_sensor);
-  
-  open_window = digitalRead(photocell_window);
-  open_door = digitalRead(photocell_door);
+  // since there's a pull-up resistor, on status corresponds to logic zero provided to the pin:
+  if( digitalRead(waiting_for_sensor) == LOW )
+    on_off = 1;
+  else
+    on_off = 0;
 
-  if (fans_status == 1)
+  // given that photocells return a logic 1 when windows and doors are closed:
+  if( digitalRead(photocell_window) == LOW /* or digitalRead(photocell_door) == LOW */ )
+    something_open = true;
+    
+  if (fans_status == 1) // aka the fans are running
     fans_timeout = fans_timeout - 1;
 }
 
